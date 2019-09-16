178d7daa4dbc1b3c2e3c7706e8138e9a94eb9423
Ben Attali <ben.attali8@gmail.com>
2019-09-16 19:26:11 +0300

    setup

--- followpol/__init__.py
+++ followpol/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'ddf0f203ed214a2d1de14957494c0f5a'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'adminlogin'
+login_manager.login_message_category = 'info'
 
 from followpol import routes
--- followpol/routes.py
+++ followpol/routes.py
@@ -1,7 +1,8 @@
-from flask import render_template, url_for, flash, redirect
-from followpol import app
+from flask import render_template, url_for, flash, redirect, request
+from followpol import app, db
 from followpol.forms import LoginForm
 from followpol.models import User
+from flask_login import login_user, current_user, logout_user, login_required
 
 @app.route('/')
 @app.route('/home')
@@ -14,15 +15,24 @@ def score():
 
 @app.route('/adminlogin', methods=['GET', 'POST'])
 def adminlogin():
+	if current_user.is_authenticated:
+		return redirect(url_for('adminpage'))
 	form = LoginForm()
 	if form.validate_on_submit():
-		if form.email.data == 'admin@cak.com' and form.password.data == '123456':
-			flash('Logged in as admin', 'success')
+		user = User.query.filter_by(email=form.email.data).first()
+		if user and form.password.data == user.password:
+			login_user(user, remember=form.remember.data)
 			return redirect(url_for('adminpage'))
 		else:
-			flash('Login unsuccessful', 'danger')
+			flash('Login unsuccessful. Please check your email and password.', 'danger')
 	return render_template('adminlogin.html', title='Admin Login', form=form)
 
+@app.route('/logout')
+def logout():
+	logout_user()
+	return redirect(url_for('home'))
+
 @app.route('/adminpage')
+@login_required
 def adminpage():
 	return render_template('adminpage.html', title='Admin Pages')
