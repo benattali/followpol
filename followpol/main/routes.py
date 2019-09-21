7fe8ce5d2b73f920d7bf218c159f017364565a61
Ben Attali <ben.attali8@gmail.com>
2019-09-21 17:34:45 +0300

    charts

--- followpol/main/routes.py
+++ followpol/main/routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, flash, url_for
 from followpol.main.forms import TwitterHandleForm
 import csv
+import pygal
 
 main = Blueprint('main', __name__)
 
@@ -19,7 +20,18 @@ def home():
             for line in csv_reader:
                 csv_data.append(line)
 
-        return render_template('score.html', twitter_handle=twitter_handle, csv_data=csv_data)
+
+        pie_chart = pygal.Pie()
+        pie_chart.title = 'Browser usage in February 2012 (in %)'
+        pie_chart.add('IE', 19.5)
+        pie_chart.add('Firefox', 36.6)
+        pie_chart.add('Chrome', 36.3)
+        pie_chart.add('Safari', 4.5)
+        pie_chart.add('Opera', 2.3)
+        pie_chart.render()
+        chart = pie_chart.render_data_uri()
+
+        return render_template('score.html', twitter_handle=twitter_handle, csv_data=csv_data, chart=chart)
     return render_template('home.html', form=form)
 
 @main.route('/score')
